d9d83b2377eccd233fc0efb30095fd6561633496
Joe Prosser <jvprosser@gmail.com>
2024-09-24 13:58:16 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!bash
-pip install pandas numpy scikit-learn seaborn
+pip install pandas numpy scikit-learn seaborn mlflow mlflow.sklearn matplotlib xgboost imblearn lightgbm 
 mkdir -p data
 touch data/features.txt
