db53de29d260d115bea4993cd13b4f4bc41a39ed
Joe Prosser <jvprosser@gmail.com>
2024-09-27 14:59:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!bash
+echo '{ "disabledExtensions": { "@jupyter/collaboration-extension": true }, "lockedExtensions": { "@jupyter/collaboration-extension": true } }' > /home/cdsw/.jupyter/labconfig/page_config.json
 pip install pandas numpy scikit-learn seaborn matplotlib xgboost imblearn lightgbm 
 mkdir -p data
