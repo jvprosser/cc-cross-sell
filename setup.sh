499f1698a88cf368d14a96e2eedaa864df2fa72a
Joe Prosser <jvprosser@gmail.com>
2024-09-28 16:29:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!bash
 echo '{ "disabledExtensions": { "@jupyter/collaboration-extension": true }, "lockedExtensions": { "@jupyter/collaboration-extension": true } }' > /home/cdsw/.jupyter/labconfig/page_config.json
 pip install pandas numpy scikit-learn seaborn matplotlib xgboost imblearn lightgbm 
+# hit a version mismatch with our pkl file
+pip install cloudpickle==3.0.0 psutil==5.9.5
 mkdir -p data
