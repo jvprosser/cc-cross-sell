83c04599066b7c91a3f84739577a569462c2b1e9
Joseph Prosser <jprosser@cloudera.com>
2024-09-30 17:09:16 +0000

    cleanup

--- cc_flask.py
+++ cc_flask.py
@@ -15,9 +15,7 @@ from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-# Load the MLflow model
-#model_path = "path/to/your/mlflow/model.pkl"
-#loaded_model = mlflow.pyfunc.load_model(model_path)
+# Load the MLflow model along with the transformers we built in the model notebook
 
 logged_model = '/home/cdsw/.experiments/b0se-2xuo-w64u-04yn/atgg-nm4g-tqs7-1r4h/artifacts/lgb_model'
 
@@ -37,29 +35,26 @@ with open('data/data_cat_cols.pkl', 'rb') as file:
     data_cat_cols = pickle.load(file) 
 
 def normalize_data(raw_data):
-    print(f"top raw_data = {raw_data}")
-    raw_data='{"Age": 27, "Vintage": 26, "Avg_Account_Balance": 707906, "Channel_Code": "X1", "Credit_Product": "No", "Gender": "Female", "Is_Active": "No", "Occupation": "Salaried", "Region_Code": "RG256"}'  
-    
-    # Later, to load the scaler:
-       
-    
-    data = json.loads(raw_data)
-    
-    cc_vector_df = pd.DataFrame([data])    
-        
+    print(f"top raw_data = {raw_data}\n")
+
+    #Testing code
+    #raw_data='{"Age": 27, "Vintage": 26, "Avg_Account_Balance": 707906, "Channel_Code": "X1", "Credit_Product": "No", "Gender": "Female", "Is_Active": "No", "Occupation": "Salaried", "Region_Code": "RG256"}'  
+    #data = json.loads(raw_data)
+    #print(f"json raw_data = {[raw_data]}")
+
+    cc_vector_df = pd.DataFrame([raw_data])    
+    print(f"raw pandas dataframe {cc_vector_df}\n")    
         
     data_num_data = cc_vector_df.loc[:, data_num_cols]
     data_cat_data = cc_vector_df.loc[:, data_cat_cols]
     
-    print("Shape of num data:", data_num_data.shape)
-    print("Shape of cat data:", data_cat_data.shape)
+    print(f"Shape of num data: {data_num_data.shape}")
+    print(f"Shape of cat data: {data_cat_data.shape}")
     
-    print(f"data_num_data: {data_num_data}")
-    print(f"data_cat_data: {data_cat_data}")
+    print(f"data_num_data: {data_num_data}\n")
+    print(f"data_cat_data: {data_cat_data}\n")
     
-    
-    #data_num_data_s = loaded_standardscaler.fit_transform(data_num_data)
-    
+        
     data_num_data_s = loaded_standardscaler.fit_transform(data_num_data)
     data_num_data_df_s = pd.DataFrame(data_num_data_s, columns = data_num_cols)
     
@@ -68,7 +63,7 @@ def normalize_data(raw_data):
     
     data_new = pd.concat([data_num_data_df_s, data_cat_data_df], axis = 1)
     
-    print(f"data new={data_new}")
+    #print(f"transformed data={data_new}\n")
     return data_new
 
 PORT = os.getenv('CDSW_APP_PORT', '8090')
@@ -114,15 +109,15 @@ def index():
             # Store results in a dictionary
             data = {
                
-                'Age': result[0],
-                'Vintage': result[1],
-                'Avg_Account_Balance': result[2],
-                'Channel_Code': result[3],
-                'Credit_Product': result[4],
-                'Gender': result[5],
-                'Is_Active': result[6],
-                'Occupation': result[7],
-                'Region_Code': result[8]
+                "Age": result[0],
+                "Vintage": result[1],
+                "Avg_Account_Balance": result[2],
+                "Channel_Code": result[3],
+                "Credit_Product": result[4],
+                "Gender": result[5],
+                "Is_Active": result[6],
+                "Occupation": result[7],
+                "Region_Code": result[8]
             }
             print(f"data dict: {data}")
 
@@ -130,7 +125,7 @@ def index():
             normalized_data = normalize_data(data)
             print(f"Normalized_data: {[normalized_data]}")
             #cc_vector_df = pd.DataFrame([normalized_data])
-            print(f"Vector df = {normalized_data}")
+            #print(f"Vector df = {normalized_data}")
             #cc_vector='{"Age":0.6831522461,"Vintage":-0.8637453118,"Avg_Account_Balance":-0.1245877441,"Channel_Code":1.0,"Credit_Product":2.0,"Gender":1.0,"Is_Active":0.0,\
             #"Occupation":3.0,"Region_Code":33.0}'          
             #data = json.loads(cc_vector)
